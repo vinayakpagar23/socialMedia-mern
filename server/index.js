60541626b376af4dff3fd45f70a0707abec29964
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-04-21 16:14:36 +0530

    user routes and controller created

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
 // Configuraations
 
 const __filename = fileURLToPath(import.meta.url);
@@ -44,11 +46,12 @@ const upload = multer({storage});
 
 /* ROUTES   WITH FILES*/
 
-app.post("/auth/register", upload.single("picture"),register);
+app.post("/auth/register", upload.single("picture"),verifyToken,register);
 
 
 /* ROUTES */
 app.use("/auth",authRoutes);
+app.use("/users",userRoutes);
 
 
 
