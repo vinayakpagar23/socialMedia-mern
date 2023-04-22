e9185cb36b4702c25f4fd968e9550fc52ff56cfd
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-04-22 10:13:28 +0530

    working fine with localhost

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          VKSOCIALAPP
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
